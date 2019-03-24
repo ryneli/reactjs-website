1a9654eed18cdd27b4b1c43b35cb7f4c621352ae
Zhenqiang Li <lizhenqiangsnake@gmail.com>
2019-03-24 19:52:44 -0400

    Pass data from child component back to parent. SearchBar => App

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
+    onSearchClicked() {
+        var searchTextBox = document.getElementById('search-textbox');
+        this.props.onClick(searchTextBox.value);
+    }
     render() {
         return (
         <div>
-            <input type='text' placeholder='eg: John3:16'></input>
-            <button onClick={() => this.props.onClick('test')}>Search</button>
+            <input id='search-textbox' type='text' placeholder='eg: John3:16'></input>
+            <button onClick={() => this.onSearchClicked()}>Search</button>
         </div>);
     }
 }
