4ddd6ec69489071fe09a4c3692eb6c867e1896e1
Zhenqiang Li <lizhenqiangsnake@gmail.cm>
2019-05-01 14:33:15 -0400

    Updates

---
+++ precache-manifest.8d2d07f83aa4b5bbbea15b64411d1e5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dee54788a2c884926ba1",
+    "url": "/bibleppt/static/js/runtime~main.ceaf2b30.js"
+  },
+  {
+    "revision": "7205d989bc77e701b5b4",
+    "url": "/bibleppt/static/js/main.35c9f888.chunk.js"
+  },
+  {
+    "revision": "880e5052ce602b7c238b",
+    "url": "/bibleppt/static/js/2.f7a296d8.chunk.js"
+  },
+  {
+    "revision": "7205d989bc77e701b5b4",
+    "url": "/bibleppt/static/css/main.2e9ef3f7.chunk.css"
+  },
+  {
+    "revision": "3ab4b443ae9a25e65d3f8ef81ac9ef7b",
+    "url": "/bibleppt/index.html"
+  }
+];
